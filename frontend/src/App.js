29cafaa4927dddb9346db3dcad5f85dd1082d6ee
Raj <rockingraj8247@gmail.com>
2023-12-16 21:42:12 +0530

    created react app for the front end part

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit'
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
